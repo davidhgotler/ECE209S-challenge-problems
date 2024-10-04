77810351bb48fb9475b4c85aaa4582b6fcb71c87
David Gotler <davidhgotler@gmail.com>
2024-10-03 18:28:29 -0700

    Added p_matrix calculation

--- gridworld.py
+++ gridworld.py
@@ -3,8 +3,44 @@ import numpy as np
 
 label_str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
+# Direction vectors for actions
+STAY = np.array((0,0))
+RIGHT = np.array((1,0))
+LEFT = np.array((-1,0))
+UP = np.array((0,1))
+DOWN = np.array((0,-1))
+actions = [STAY,RIGHT,LEFT,UP,DOWN]
+
+
+@dataclass
+class state:
+    label:str
+    coord:np.ndarray
+
+@dataclass
+class destination(state):
+    destination_type:str = 'ice cream shop'
+
+@dataclass
+class obstacle(state):
+    pass
+        
+@dataclass
+class hazard(state):
+    pass
+
+class agent:
+    def __init__(self,s:state,history:list[state]=[]) -> None:
+        self.s = s
+        self.history = history
+
+    def update_state(self,s_new):
+        self.history.append(self.s)
+        self.s = s_new
+
+
 class gridworld:
-    def __init__(self,size:tuple[int],agent_start:tuple[int],destinations:list[tuple[int]],obstacles:list[tuple[int]],hazards:list[tuple[int]],destination_type='ice cream shop') -> None:
+    def __init__(self,size:tuple[int],agent_start:np.ndarray,destinations:list[tuple[int]],obstacles:list[tuple[int]],hazards:list[tuple[int]],p_e = 0.2,destination_type='ice cream shop') -> None:
         '''
         `size` = size of the grid-world in X,Y\\
         `agent_start` = (x,y) coordinate for the starting position of the agent\\
@@ -19,15 +55,22 @@ class gridworld:
             self.num_states = size[0]*size[1]
         else:
             raise Exception("size should be integer valued tuple lenth 2")
+        # Initialize/calc grid states
         self.make_grid(destinations,obstacles,hazards,destination_type)
+        self.p_e = p_e # prob of error
+        # transition probabilities
+        self.calc_p_matrix()
+
         if len(agent_start) == 2:
             # Check start coord to be in bounds
-            if agent_start in self.state_coords:
-                self.agent = agent(self.coord2state(agent_start))
-
+            s_start = self.coord2state(agent_start)
+            if s_start and s_start is not obstacle:
+                self.agent = agent(s_start)
+            else:
+                raise Exception("could not find valid state from agent_start")
         else:
             raise Exception("agent_start should be integer valued tuple of length 2")
-    
+
     def make_grid(self,destinations,obstacles,hazards,destination_type):
         self.states = []
         self.state_labels = []
@@ -36,56 +79,57 @@ class gridworld:
             x = i%self.x_max
             y = i//self.x_max
             self.state_labels.append(label)
-            self.state_coords.append((x,y))
+            self.state_coords.append(np.array((x,y)))
             if (x,y) in destinations:
-                self.states.append(destination(label,x,y,destination_type))
+                self.states.append(destination(label,np.array((x,y)),destination_type))
             elif (x,y) in obstacles:
-                self.states.append(obstacle(label,x,y))
+                self.states.append(obstacle(label,np.array((x,y))))
             elif (x,y) in hazards:
-                self.states.append(hazard(label,x,y))
+                self.states.append(hazard(label,np.array((x,y))))
             else:
-                self.states.append(state(label,x,y))
+                self.states.append(state(label,np.array((x,y))))
 
     def coord2state(self,coord):
         for state in self.states:
-            if coord == (state.x,state.y):
+            if np.all(coord==state.coord):
                 return state
+        return
+        
     def label2state(self,label):
         for state in self.states:
             if label == state.label:
                 return state
-
-@dataclass
-class state:
-    label:str
-    x:int
-    y:int
-
-@dataclass
-class destination(state):
-    destination_type:str = 'ice cream shop'
-
-class obstacle(state):
-    def __init__(self,label,x,y):
-        super().__init__(label,x,y)
-        
-class hazard(state):
-    def __init__(self,label,x,y):
-        super().__init__(label,x,y)
-
-class agent:
-    def __init__(self,s:state,history:list[state]=[]) -> None:
-        self.s = s
-        self.history = history
-
-        # Direction vectors for actions
-        self.RIGHT = (1,0)
-        self.UP = (0,1)
-        self.LEFT = (-1,0)
-        self.DOWN = (0,-1)
-        self.STAY = (0,0)
-
-    def update_state(self,s_new):
-        self.history.append(self.s)
-        self.s = s_new
+        return
+    
+    # def update_action_set(self):
+    #     self.action_set = []
+    #     for action in actions:
+    #         s_new = self.coord2state((self.state.x+action[0],self.state.y+action[1]))
+    #         if s_new is obstacle:
+    #             continue
+    #         if s_new in self.states:
+    #             self.action.append(action)
+    
+    def calc_p_matrix(self):
+        self.p_matrix = np.zeros((len(actions),self.num_states,self.num_states))
+        for i,a_t in enumerate(actions):
+            for j,s_t in enumerate(self.states):
+                for k,s_tp1 in enumerate(self.states):
+                    # boundaries and obstacles
+                    s_calc = self.coord2state(s_t.coord + a_t)
+                    if not s_calc or (s_calc is obstacle):
+                        # not available state - add to stay probability
+                        self.p_matrix[0,j,k] += self.p_e/(len(actions)-1)
+                    # intended direction
+                    elif np.all(s_t.coord + a_t == s_tp1.coord):
+                        self.p_matrix[i,j,k] += 1 - self.p_e
+                    # off-angles within one square
+                    # Check if distance is in the action set i.e. within one orthogonal step
+                    if np.any(np.all((s_tp1.coord - s_t.coord)==actions,axis=1)):
+                        self.p_matrix[i,j,k] += self.p_e/(len(actions)-1)
+                    # states that are more than 1 action away
+                    else:
+                        self.p_matrix[i,j,k] += 0 # redundant
+                        
+                
 

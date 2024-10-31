9f3da9b99ba49cfdfe137d1ba07597dff3bfe446
David Gotler <davidhgotler@gmail.com>
2024-10-31 13:01:31 -0700

    set up lqr solver class

--- numberline/lqr.py
+++ numberline/lqr.py
@@ -1,8 +1,15 @@
 import numpy as np
+from copy import copy
 
 class lqr:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,A,B,Q,R) -> None:
+        self.A = A
+        self.B = B
+        self.Q = Q
+        self.R = R
+        self.P = copy(self.Q)
 
     def solver(self):
         pass
+        # calc K
+        # calc P
